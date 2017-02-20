dfa8b016b44b1d454514998024818a6b379e9247
Chris <christophersstjohn@gmail.com>
2017-02-20 15:01:34 +0700

    Adding BUTTON, Nacho-UI component

--- ex1/index.android.js
+++ ex1/index.android.js
@@ -1,17 +1,52 @@
 import React from 'react';
 import {
   AppRegistry,
-  Text,
+  Text, View
 } from 'react-native';
+import {Button} from 'nachos-ui';
 // import App from './src/App'
 import { StackNavigator } from 'react-navigation';
 
 class HomeScreen extends React.Component {
+
   static navigationOptions = {
     title: 'Welcome to EX1',
   };
+
+  constructor(props){
+      super(props);
+      this.state = { onButton:1 };
+  }
+
+  onButtonPress(){
+    const buttonOnStatus = this.state.onButton;
+    this.setState({onButton:this.state.onButton+1});
+    return (alert(buttonOnStatus));
+  }
+
   render() {
-    return <Text style={{padding:20, fontSize: 30}}>Hello, this is EX1 Navigation!</Text>;
+    return (
+      <View>
+        <View>
+          <Text style={{padding:20, fontSize: 30}}>
+          Hello, this is EX1 Navigation!
+          </Text>
+        </View>
+        <View>
+          <Text style={{padding:20, fontSize: 20}}>
+            We're testing out this app to see if it works
+          </Text>
+        </View>
+        <View>
+          <Button
+            kind='squared'
+            style={{ marginLeft:40, marginRight:40}}
+            type='primary'
+            onPress={this.onButtonPress.bind(this)}
+          >This is a button</Button>
+        </View>
+      </View>
+    );
   }
 }
 
