5b4f1af64fea1fff1a876bc785ba0edf4b9c00ca
Chris <christophersstjohn@gmail.com>
2017-02-20 16:02:44 +0700

    Add Navigation, Buttons

--- ex1/index.android.js
+++ ex1/index.android.js
@@ -25,6 +25,7 @@ class HomeScreen extends React.Component {
   }
 
   render() {
+    const { navigate } = this.props.navigation;
     return (
       <View>
         <View>
@@ -37,21 +38,54 @@ class HomeScreen extends React.Component {
             We're testing out this app to see if it works
           </Text>
         </View>
-        <View>
+        <View style={{marginTop:20}}>
           <Button
+            key="1"
             kind='squared'
-            style={{ marginLeft:40, marginRight:40}}
+            style={{ marginLeft:50, marginRight:50}}
+            textStyle={{ fontSize:20}}
             type='primary'
             onPress={this.onButtonPress.bind(this)}
-          >This is a button</Button>
+          >Increment Stuff
+          </Button>
         </View>
+        <View>
+          <Button
+            key="2"
+            kind='squared'
+            style={{ marginTop: 60, marginLeft:50, marginRight:50}}
+            textStyle={{ fontSize:20}}
+            type='danger'
+            onPress={() => navigate('Chat', { user: 'Chris' })}
+          >Chat with Chris
+        </Button>
+        </View>
+
       </View>
     );
   }
 }
 
+class ChatScreen extends React.Component {
+  static navigationOptions = {
+    // Nav options can be defined as a function of the navigation prop:
+    title: ({ state }) => `Chat with ${state.params.user}`,
+  };
+  render() {
+    // The screen's current route is passed in to `props.navigation.state`:
+    const { params } = this.props.navigation.state;
+    return (
+      <View>
+        <Text>Chat with {params.user}</Text>
+      </View>
+    );
+  }
+}
+
+
 const ex1 = StackNavigator({
   Home: { screen: HomeScreen },
+  Chat: { screen: ChatScreen },
 });
 
 AppRegistry.registerComponent('ex1', () => ex1);
